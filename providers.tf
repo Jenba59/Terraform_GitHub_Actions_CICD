affdaf9bf1e45d452d4ec7165ff75ccba12913b7
Jose de la Barrera <118834023+Jenba59@users.noreply.github.com>
2026-03-03 10:01:29 -0600

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -5,10 +5,10 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "neeha-s3bucket-date"
+    bucket         = "jose-s3bucket-01april"
     key            = "globalstate/s3/terraform.tfstate"
     region         = "eu-west-1"
-    dynamodb_table = "neeha-date-dblocks"
+    dynamodb_table = "jose-01april-dblocks"
     encrypt        = true
   }
 }
